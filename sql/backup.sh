4b5dcad8808ca8ecd3dd185ae48ec5d7f6cbce4b
sole <sole@aec492a4-b55d-4219-85ae-fd67ab178acf>
2010-06-30 20:51:45 +0000

    subo la nueva base
    git-svn-id: https://svn.gcoop.com.ar/gestioncooperativa/trunk@13 aec492a4-b55d-4219-85ae-fd67ab178acf

--- sql/backup.sh
+++ sql/backup.sh
@@ -3,7 +3,7 @@
 DB_HOST="localhost"
 DB_NAME="gestion"
 DB_USER="root"
-DB_PASS=""
+DB_PASS="latribu"
 
 mysqldump -h $DB_HOST --user=$DB_USER --password=$DB_PASS -B $DB_NAME --opt > $DB_NAME.sql
 gzip $DB_NAME.sql
