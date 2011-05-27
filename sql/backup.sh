0490eb58ffa1cf5c7705a12f607bc8e5ca297dff
Luciano Rossi <lukio@gcoop.coop>
2011-05-27 14:59:14 -0300

    Borro archivos de sql que ya no son necesarios

--- sql/backup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-DB_HOST="localhost"
-DB_NAME="gestion"
-DB_USER="root"
-DB_PASS="latribu"
-
-mysqldump -h $DB_HOST --user=$DB_USER --password=$DB_PASS -B $DB_NAME --opt > $DB_NAME.sql
-gzip $DB_NAME.sql
